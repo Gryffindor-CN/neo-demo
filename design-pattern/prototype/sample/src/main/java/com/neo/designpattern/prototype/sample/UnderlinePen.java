0d825e92c3199bf469f54c3f067d8a95228d86e1
cpChen <chenpeidong@lonntec.com>
2019-03-14 17:31:33 +0800

    prototype

---
+++ design-pattern/prototype/sample/src/main/java/com/neo/designpattern/prototype/sample/UnderlinePen.java
@@ -0,0 +1,33 @@
+package com.neo.designpattern.prototype.sample;
+
+import com.neo.designpattern.prototype.sample.framework.Product;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int length = s.getBytes().length;
+        System.out.print("\"" + s + "\"");
+        System.out.println(" ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(ulchar);
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public Product createclone() {
+        Product p = null;
+        try {
+            p = (Product) this.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
